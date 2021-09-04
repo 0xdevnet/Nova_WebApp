d1661d9596efe52e291b2bdb5f9b1c534f4f3031
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-09-04 16:21:35 +0900

    deploy test

--- deploy/scripts/main.ts
+++ deploy/scripts/main.ts
@@ -9,7 +9,7 @@ import * as fs from "fs";
 
 (async () => {
   try {
-    const mnemonic_key = new MnemonicKey({
+    const mk = new MnemonicKey({
       mnemonic: process.env.TERRA_MNEMOMIC_KEY,
     });
 
@@ -18,28 +18,33 @@ import * as fs from "fs";
       URL: "https://tequila-lcd.terra.dev",
     });
 
-    const wallet = terra.wallet(mnemonic_key);
+    const wallet = terra.wallet(mk);
 
     const storeCode = new MsgStoreCode(
       wallet.key.accAddress,
-      fs.readFileSync("../artifacts/sample-contract.wasm").toString("base64")
+      fs.readFileSync("../artifacts/sample_contract.wasm").toString("base64")
     );
+    const storeCodeTx = await wallet.createAndSignTx({
+      msgs: [storeCode],
+    });
+    const storeCodeTxResult = await terra.tx.broadcast(storeCodeTx);
 
-    const storeCodeTransact = await wallet.createAndSignTx({ msgs: [storeCode], });
-    const storeCodeTxResult = await terra.tx.broadcast(storeCodeTransact);
+    console.log(storeCodeTxResult);
 
-    if ( isTxError(storeCodeTxResult) )
-      throw new Error (
-        `store code failed-
-        code:      ${storeCodeTxResult.code},
-        codespace: ${storeCodeTxResult.codespace},
-        raw_log:   ${storeCodeTxResult.raw_log}`
-      )
-    else console.log(storeCodeTxResult)
+    if (isTxError(storeCodeTxResult)) {
+      throw new Error(
+        `store code failed. 
+        code: ${storeCodeTxResult.code}, 
+        codespace: ${storeCodeTxResult.codespace}, 
+        raw_log: ${storeCodeTxResult.raw_log}`
+      );
+    }
 
-    const { store_code: { code_id }, } = storeCodeTxResult.logs[0].eventsByType;
+    const {
+      store_code: { code_id },
+    } = storeCodeTxResult.logs[0].eventsByType;
 
-    console.log("Code ID: ", code_id);
+    console.log("code_id", code_id);
 
     const code_id_num = Number.parseFloat(code_id[0]);
 
@@ -47,29 +52,33 @@ import * as fs from "fs";
       wallet.key.accAddress,
       code_id_num,
       {
-        count: 5,
+      count: 5,
       },
-      { uluna: 100, ukrw: 100 } // initialize coins
+      { uluna: 100, ukrw: 100 } // init coins
     );
 
-    console.log("instantiate: ", instantiate);
+    console.log("instantiate", instantiate);
 
     const instantiateTx = await wallet.createAndSignTx({
       msgs: [instantiate],
     });
 
-    console.log("instantiateTx: ", instantiateTx);
+    console.log("instantiateTx", instantiateTx);
 
     const instantiateTxResult = await terra.tx.broadcast(instantiateTx);
 
-    if (isTxError(instantiateTxResult))
+    console.log("instantiateTxResult ", instantiateTxResult);
+
+    console.log(instantiateTxResult);
+
+    if (isTxError(instantiateTxResult)) {
       throw new Error(
         `instantiate failed. 
-        code:      ${instantiateTxResult.code}, 
+        code: ${instantiateTxResult.code}, 
         codespace: ${instantiateTxResult.codespace}, 
-        raw_log:   ${instantiateTxResult.raw_log}`
-      )
-    else console.log(instantiateTxResult);
+        raw_log: ${instantiateTxResult.raw_log}`
+      );
+    }
 
     const {
       instantiate_contract: { contract_address },
