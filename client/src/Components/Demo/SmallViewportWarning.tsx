cb70cbf1b48fc57a6b6736d3488bc21d8c63af83
brianlee1597 <76828418+brianlee1597@users.noreply.github.com>
2021-09-09 13:17:47 +0900

    added smallviewport block

---
+++ client/src/Components/Demo/SmallViewportWarning.tsx
@@ -0,0 +1,6 @@
+const SmallViewPortWarning = () => 
+    <div style={{height: '100vh', display: 'grid', placeItems: 'center'}}>
+        Please visit this page on a PC Web Browser!
+    </div>
+
+export default SmallViewPortWarning
